7dc65d220323912c5c2ccde9fd39c7640feeb795
Sebastian Giulianelli <sebastiangiulianelli02@gmail.com>
2024-02-17 19:47:13 -0300

    Update place.js

--- backend/controllers/place.js
+++ backend/controllers/place.js
@@ -83,7 +83,7 @@ export class PlaceController {
         const placeData = req.body;
         delete placeData.user_id
 
-        const img = "http://192.168.0.19:1234/profile-images/" + file.filename;
+        const img = "http://mercadoqr-server.onrender.com/profile-images/" + file.filename;
 
         const result = validatePlace({img, ...placeData});
 
