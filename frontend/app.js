28935d4d507662c38a326e98c8b69c7ce2059f66
= <sebastiangiulianelli02@gmail.com>
2024-02-17 05:42:49 -0300

    node app

---
+++ frontend/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+app.listen(process.env.REACT_APP_PORT, () => {
+    console.log("Escuchando en puerto" + process.env.REACT_APP_PORT)
+});
